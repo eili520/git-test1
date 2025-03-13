97c6b3a6ca7471426c187b91adb20c694a0f33b5
eili <3325782963@qq.com>
2025-03-13 21:00:24 +0800

    second commit

--- src/main/java/com/itheima/Main.java
+++ src/main/java/com/itheima/Main.java
@@ -3,5 +3,6 @@ package com.itheima;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello git! fff");
+        System.out.println("Hello git! ggg");
     }
 }
